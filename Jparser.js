e8c5f77d1ee40931d58f02e44bd9df78c5d3f48c
AishwaryaChat <chaturvedi.aishwarya@gmail.com>
2017-01-27 12:23:43 +0530

    JSON Parser completed

--- Jparser.js
+++ Jparser.js
@@ -1,39 +1,9 @@
-var input = '{"glossary": {   "title"   : "example glossary","GlossDiv": {"title": "S","GlossList": {"GlossEntry": {"ID": "SGML","SortAs": "SGML","GlossTerm": "Standard Generalized Markup Language","Acronym": "SGML","Abbrev": "ISO 8879:1986","GlossDef": {"para": "A meta-markup language, used to create markup languages such as DocBook.","GlossSeeAlso": ["GML", "XML"]},"GlossSee": "markup"}}}}}'
-
-function nullParser (input) {
-  if (input.slice(0, 4) === 'null') {
-    return [null, input.slice(4)]
-  } return null
-}
-
-function boolParser (input) {
-  if (input.slice(0, 4) === 'true') {
-    return [true, input.slice(4)]
-  } else if (input.slice(0, 5) === 'false') {
-    return [false, input.slice(5)]
-  }
-  return null
-}
-
-function commaParser (input) {
-  if (input.slice(0, 1) === ',') {
-    return [',', input.slice(1)]
-  }
-  return null
-}
-
-function colonParser (input) {
-  if (input.slice(0, 1) === ':') {
-    return [':', input.slice(1)]
-  }
-  return null
-}
-
-function spaceParser (input) {
-  if (!input.match(/^[\s\n]/)) return null
-  let regexp = input.slice(input.match(/\S/).index)
-  return [null, regexp]
-}
+var input = '{"glossary":         {"title"   : "example glossary"         ,"GlossDiv": {"title": "S","GlossList": {"GlossEntry": {"ID": "SGML","SortAs": "SGML","GlossTerm": "Standard Generalized Markup Language","Acronym": "SGML","Abbrev": "ISO 8879:1986","GlossDef": {"para": "A meta-markup language, used to create markup languages such as DocBook.","GlossSeeAlso": ["GML", "XML"]},"GlossSee": "markup"}}}}}'
+const nullParser = input => input.startsWith('null') ? [null, input.slice(4)] : null
+const boolParser = input => input.startsWith('true') ? ['true', input.slice(4)] : (input.startsWith('false') ? ['false', input.slice(5)] : null)
+const commaParser = input => input.startsWith(',') ? [',', input.slice(1)] : null
+const colonParser = input => input.startsWith(':') ? [':', input.slice(1)] : null
+const spaceParser = input => input.match(/^[\s\n]/) ? [null, input.slice(input.match(/\S/).index)] : null
 
 function stringParser (input) {
   let i = 1
@@ -60,24 +30,21 @@ function numParser (input) {
 }
 
 function arrayParser (input) {
+  let result
   let s = []
   if (!input.startsWith('[')) {
     return null
   }
   input = input.slice(1)
-  let space = spaceParser(input)
-  if (space !== null) {
-    input = space[1]
-  }
   while (true) {
-    let result = valueParser(input)
-    if (result === null) return null
+    result = spaceParser(input)
+    if (result !== null) input = result[1]
+    result = valueParser(input)
+    if (result === null) break
     s.push(result[0])
     input = result[1]
-    space = spaceParser(input)
-    if (space !== null) {
-      input = space[1]
-    }
+    result = spaceParser(input)
+    if (result !== null) input = result[1]
     result = commaParser(input)
     if (result === null) break
     input = result[1]
@@ -85,6 +52,8 @@ function arrayParser (input) {
       return null
     }
   }
+  result = spaceParser(input)
+  if (result !== null) input = result[1]
   if (input.startsWith(']')) {
     return [s, input.slice(1)]
   }
@@ -94,39 +63,32 @@ function arrayParser (input) {
 function objectParser (input) {
   let obj = {}
   let key = ''
-  let value = ''
-  if (!input.startsWith('{')) {
-    return null
-  }
+  let value
+  let result
+  if (!input.startsWith('{')) return null
   input = input.slice(1)
-  let space = spaceParser(input)
-  if (space !== null) {
-    input = space[1]
-    console.log(input)
-  }
   while (true) {
-    let result = stringParser(input)
+    result = spaceParser(input)
+    if (result !== null) input = result[1]
+    result = stringParser(input)
     if (result === null) break
     else {
       key = result[0]
       input = result[1]
-      space = spaceParser(input)
-      if (space !== null) {
-        input = space[1]
-      }
+      result = spaceParser(input)
+      if (result !== null) input = result[1]
       result = colonParser(input)
       if (result === null) return null
       input = result[1]
-      space = spaceParser(input)
-      if (space !== null) {
-        input = space[1]
-      }
+      result = spaceParser(input)
+      if (result !== null) input = result[1]
       result = valueParser(input)
       if (result === null) return null
       value = result[0]
-      input = result[1]
       obj[key] = value
       input = result[1]
+      result = spaceParser(input)
+      if (result !== null) input = result[1]
       result = commaParser(input)
       if (result === null) break
       input = result[1]
@@ -135,36 +97,23 @@ function objectParser (input) {
       }
     }
   }
-  if (input.startsWith('}')) {
-    // console.log(obj)
-    return [obj, input.slice(1)]
-  }
-  return null
+  result = spaceParser(input)
+  if (result !== null) input = result[1]
+  if (!input.startsWith('}')) return null
+  return [obj, input.slice(1)]
 }
 
 function valueParser (input) {
-  let result = input
-  while (result !== null) {
-    if (arrayParser(input) !== null) {
-      result = arrayParser(input)
-      input = result[1]
-    } else if (objectParser(input) !== null) {
-      result = objectParser(input)
-      input = result[1]
-    } else if (numParser(input) !== null) {
-      result = numParser(input)
-      input = result[1]
-    } else if (stringParser(input) !== null) {
-      result = stringParser(input)
-      input = result[1]
-    } else if (boolParser(input) !== null) {
-      result = boolParser(input)
-    } else if (nullParser(input) !== null) {
-      result = nullParser(input)
-      input = result[1]
-    } else return result
-  }
+  let result
+  result = objectParser(input) || arrayParser(input) || stringParser(input) || numParser(input) || nullParser(input) || boolParser(input)
+  return result
 }
 
 let output = valueParser(input)
+if (output === null) output = 'Invalid JSON'
+else {
+  let result = output[1].trim()
+  if (result.length !== 0) output = 'Invalid JSON'
+  else output = output[0]
+}
 console.log(output)
